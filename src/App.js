31941425670d4fba9251236412525c23ee0ad7e3
Dattu Naik G <gugulothdattu000@gmail.com>
2022-08-05 19:31:29 +0000

    created Login File

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 import SignUp from "./SignUp";
+import Login from './Login';
 export default function App() {
   return (
     <div className="App">
       <SignUp />
+      <Login />
     </div>
   );
 }
